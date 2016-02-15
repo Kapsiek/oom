596b6892a90906fa63b4e30739f9a0019f7f53cb
kapsiek <ic15b081@ic15b081-NB.kapsiek>
2016-02-15 23:01:54 +0100

    add project Hello_World

---
+++ tasks/Hello_World/Hello_World/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Hello_World
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string name;
+			Console.WriteLine ("Hello! Whats your name?\n");
+			name = Console.ReadLine();
+			Console.WriteLine("\nHello {0} \n", name);
+			Console.ReadLine ();
+		}
+	}
+}
